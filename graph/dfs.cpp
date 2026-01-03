16ea5c8f2cd34743ab0f78bd921979f287319521
dishamishra1 <dishamishra8267@gmail.com>
2026-01-03 20:45:32 +0530

    Create dfs.cpp

---
+++ graph/dfs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+  
+   void dfs( int node, vector<vector<int>>& adj  , int vis[] , vector<int> &ls)
+   {
+       vis[node]=1;
+       ls.push_back(node);
+       for(auto it: adj[node])
+       {
+           if(!vis[it])
+           {
+               dfs(it,adj,vis,ls);
+           }
+       }
+       
+   }
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int v=adj.size();
+        int vis[v]={0};
+        int start=0;
+        vector<int> ls;
+        dfs(start,adj,vis,ls);
+        return ls;
+        
+    }
+};
