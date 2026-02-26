8a13a975e559a7ca0bcd75b78020c34e24fb042a
dishamishra1 <dishamishra8267@gmail.com>
2026-02-26 23:53:26 +0530

    Create implement stack using queue.cpp

---
+++ stack and queue/implement stack using queue.cpp
@@ -0,0 +1,39 @@
+class MyStack {
+     queue<int> q;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x)
+    {
+        int size=q.size();
+        q.push(x);
+        for(int i=0 ; i<size ; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        
+    }
+    
+    int pop() 
+    {
+        int x=q.front();
+        q.pop();
+        return x;
+        
+    }
+    
+    int top() {
+        return q.front();
+        
+    }
+    
+    bool empty() {
+       return q.empty();
+
+
+        
+    }
+};
