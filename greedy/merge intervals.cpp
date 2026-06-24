6517032cc9f1e721d825b0a506ca10a9f8bb92f3
dishamishra1 <dishamishra8267@gmail.com>
2026-06-24 17:00:01 +0530

    Create merge intervals.cpp

---
+++ greedy/merge intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals)
+    {
+        vector<vector<int>> ans;
+        int n=intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<int> temp=intervals[0];
+
+        for(auto it: intervals)
+        {
+            if(it[0]<=temp[1])
+            {
+                temp[1]=max(it[1],temp[1]);
+
+            }
+            else
+            {
+                ans.push_back(temp);
+                temp=it;
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+
+
+        
+    }
+};
