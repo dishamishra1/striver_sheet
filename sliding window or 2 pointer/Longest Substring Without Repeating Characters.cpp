b6bc595bf8ffa8036882a14af68f40d933f4cf11
dishamishra1 <dishamishra8267@gmail.com>
2026-07-06 22:14:57 +0530

    Create Longest Substring Without Repeating Characters.cpp

---
+++ sliding window or 2 pointer/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        int n=s.size();
+        int maxi=0;
+        for(int i=0 ; i< n ; i++)
+        {
+            vector<int> hash(256,0);
+            for(int j=i ; j<n ; j++)
+            {
+                if(hash[s[j]]==1) break;
+                int len=j-i+1;
+                hash[s[j]]=1;
+                maxi=max(len,maxi);
+            }
+        }
+        return maxi;
+        
+    }
+};
